4ab4bef16422152f91e19cd249794e540cc7fd91
Ericke Gallardo <stratcomm@ustp.edu.ph>
2022-09-23 11:28:00 +0800

    revised navigation on flagpole area

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.13/18080
 // Filename: panaon.ggsk
-// Generated 2022-09-23T11:23:25
+// Generated 2022-09-23T11:27:25
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
